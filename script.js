f0eeca352c0b505a5b91fdf66a79d93931de4d8f
Foysal Ahmed <85146993+foysal4O4@users.noreply.github.com>
2024-10-26 20:10:25 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-function playSound() {
+function playMusic() {
+    const birthdayMusic = document.getElementById('birthdayMusic');
+    birthdayMusic.play();
+}
+
+function showWish() {
     const specialWishDiv = document.getElementById('specialWish');
-    const birthdaySong = document.getElementById('birthdaySong');
+    const birthdaySound = document.getElementById('birthdaySound');
 
     specialWishDiv.textContent = "Happy Birthday, Fazly Rabby! 🎉 May your day be filled with love and happiness!";
     specialWishDiv.classList.remove('hidden');
     
-    birthdaySong.play();
+    birthdaySound.play();
 }
